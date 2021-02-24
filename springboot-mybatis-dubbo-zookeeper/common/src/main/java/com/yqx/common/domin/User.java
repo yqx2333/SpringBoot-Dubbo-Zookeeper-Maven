1c4fdb790438881ab89fa979953e465de15b0af4
YuQingXi <840174265@qq.com>
2021-02-24 10:52:04 +0800

    搭建好SpringBoot+Dubbo+Zookeeper+Maven项目框架

---
+++ springboot-mybatis-dubbo-zookeeper/common/src/main/java/com/yqx/common/domin/User.java
@@ -0,0 +1,8 @@
+package com.yqx.common.domin;
+
+import java.io.Serializable;
+
+// 在Common工程中创建User实体类对象 并实现Serializable接口
+// 表明可以让该类进行序列化 无具体意义.
+public class User implements Serializable {
+}
---
+++ springboot-mybatis-dubbo-zookeeper/common/src/main/java/com/yqx/common/service/UserService.java
@@ -0,0 +1,5 @@
+package com.yqx.common.service;
+
+// 在Common工程中创建UserService接口
+public interface UserService {
+}
