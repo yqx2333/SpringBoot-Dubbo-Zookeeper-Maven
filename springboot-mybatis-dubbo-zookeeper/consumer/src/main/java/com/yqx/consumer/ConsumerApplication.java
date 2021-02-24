c9d2de226db3f56339db45dcdc9a9564b014efaf
YuQingXi <840174265@qq.com>
2021-02-24 17:37:45 +0800

    Consumer远程调用查询数据成功

--- springboot-mybatis-dubbo-zookeeper/consumer/src/main/java/com/yqx/consumer/ConsumerApplication.java
+++ springboot-mybatis-dubbo-zookeeper/consumer/src/main/java/com/yqx/consumer/ConsumerApplication.java
@@ -1,9 +1,13 @@
 package com.yqx.consumer;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@EnableDubbo        // 开启Dubbo注解支持
+@ImportResource(locations = "classpath:spring-dubbo.xml" ) // 扫描spring-dubbo.xml文件
 public class ConsumerApplication {
 
     public static void main(String[] args) {
